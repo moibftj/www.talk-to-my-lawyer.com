ac86205488441c3e303c7362c7bf149f444dd281
Moiz Jamil <moizjmj@icloud.com>
2025-12-29 11:47:38 +0000

    chore: add git configuration script for user setup

---
+++ set-git-config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name "Moiz Jamil"
+git config --global user.email "moizjmj@icloud.com"
+echo "Git configuration updated:"
+echo "Name: $(git config --global user.name)"
+echo "Email: $(git config --global user.email)"
